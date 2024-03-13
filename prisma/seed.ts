b0cfbafb925b8077b170e6a3d0e6ef6a152597fb
oh-ji-yeon <o.delayed0106@gmail.com>
2024-03-13 13:32:50 +0900

    React Query로 데이터 가져오기

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,33 @@
 import { PrismaClient } from "@prisma/client";
-import * as data from "../src/data/store_data.json";
+// import * as data from "../src/data/store_data.json";
 
 const prisma = new PrismaClient();
 
-async function seedData() {
-  data?.["DATA"]?.map(async (store) => {
-    // data에 DATA를 가져와 배열을 맵핑하면서 원하는 필드만 추가
-    const storeData = {
-      // storeData 정리
-      phone: store?.tel_no,
-      address: store?.rdn_code_nm,
-      lat: store?.y_dnts,
-      lng: store?.x_cnts,
-      name: store?.upso_nm,
-      category: store?.bizcnd_code_nm,
-      storeType: store?.cob_code_nm,
-      foodCertifyName: store?.crtfc_gbn_nm,
-    };
+// async function seedData() {
+//   data?.["DATA"]?.map(async (store) => {
+//     // data에 DATA를 가져와 배열을 맵핑하면서 원하는 필드만 추가
+//     const storeData = {
+//       // storeData 정리
+//       phone: store?.tel_no,
+//       address: store?.rdn_code_nm,
+//       lat: store?.y_dnts,
+//       lng: store?.x_cnts,
+//       name: store?.upso_nm,
+//       category: store?.bizcnd_code_nm,
+//       storeType: store?.cob_code_nm,
+//       foodCertifyName: store?.crtfc_gbn_nm,
+//     };
 
-    // storeData 생성
-    const res = await prisma.store.create({
-      data: storeData,
-    });
-    console.log(res);
-  });
-}
+//     // storeData 생성
+//     const res = await prisma.store.create({
+//       data: storeData,
+//     });
+//     console.log(res);
+//   });
+// }
 
 async function main() {
-  await seedData();
+  // await seedData();
 }
 
 main()
