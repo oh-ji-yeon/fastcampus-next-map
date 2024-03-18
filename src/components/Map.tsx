97675129e8ab8cc6ab26c6c9298129f9822a7ca4
oh-ji-yeon <o.delayed0106@gmail.com>
2024-03-18 16:59:21 +0900

    상세페이지 지도 띄우기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -12,18 +12,26 @@ declare global {
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_LNG = 127.03088379;
 
+const DEFAULT_ZOOM = 3;
+
 interface MapProps {
   setMap: Dispatch<SetStateAction<any>>;
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
 }
 
-export default function Map({ setMap }: MapProps) {
+export default function Map({ setMap, lat, lng, zoom }: MapProps) {
   const loadKakaoMap = () => {
     // kakao map 로드
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG), // 지도 중심 좌표 - 위도,경도
-        level: 3, // 지도 확대 레벨
+        center: new window.kakao.maps.LatLng(
+          lat ?? DEFAULT_LAT,
+          lng ?? DEFAULT_LNG
+        ), // 지도 중심 좌표 - 위도,경도
+        level: zoom ?? DEFAULT_ZOOM, // 지도 확대 레벨
       };
 
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
