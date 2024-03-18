7777e2b93c8ffa205787ba2b15790d39b4541854
oh-ji-yeon <o.delayed0106@gmail.com>
2024-03-18 14:15:44 +0900

    Kakao Login 실습

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
         <div className="flex flex-col gap-3">
           <button
             type="button"
-            onClick={() => signIn("google")}
+            onClick={() => signIn("google", { callbackUrl: "/" })}
             className="text-white flex gap-4 bg-[#4285f4] hover:bg-[#4285f4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
           >
             <AiOutlineGoogle className="w-6 h-6" />
@@ -44,7 +44,7 @@ export default function LoginPage() {
           </button>
           <button
             type="button"
-            onClick={() => signIn("naver")}
+            onClick={() => signIn("naver", { callbackUrl: "/" })}
             className="text-white flex gap-3 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
           >
             <SiNaver className="w-4 h-4" />
@@ -52,6 +52,7 @@ export default function LoginPage() {
           </button>
           <button
             type="button"
+            onClick={() => signIn("kakao", { callbackUrl: "/" })}
             className="text-black flex gap-4 bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
           >
             <RiKakaoTalkFill className="w-6 h-6" />
