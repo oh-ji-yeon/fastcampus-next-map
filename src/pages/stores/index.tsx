bed0f4ac79f2a13ce77c49c94bb30a83bc348738
oh-ji-yeon <o.delayed0106@gmail.com>
2024-03-19 13:13:05 +0900

    검색&정렬 구현하기

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import Image from "next/image";
 import { StoreApiResponse, StoreType } from "@/interface";
@@ -7,25 +7,31 @@ import { useInfiniteQuery, useQuery } from "react-query";
 
 import axios from "axios";
 import Loading from "@/components/Loading";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Pagination from "@/components/Pagination";
+
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import Loader from "@/components/Loader";
 import SearchFilter from "@/components/SearchFilter";
 
 export default function StoreListPage() {
-  const router = useRouter();
-  const { page = "1" }: any = router.query;
   const ref = useRef<HTMLDivElement | null>(null);
   const pageRef = useIntersectionObserver(ref, {});
   const isPageEnd = !!pageRef?.isIntersecting;
+  const [q, setQ] = useState<string | null>(null);
+  const [district, setDistrict] = useState<string | null>(null);
+
+  const searchParams = {
+    q: q,
+    district: district,
+  };
+
+  // console.log(searchParams);
 
   const fetchStores = async ({ pageParam = 1 }) => {
     const { data } = await axios("/api/stores?page=" + pageParam, {
       params: {
         limit: 10,
         page: pageParam,
+        ...searchParams,
       },
     });
 
@@ -40,7 +46,7 @@ export default function StoreListPage() {
     hasNextPage,
     isError,
     isLoading,
-  } = useInfiniteQuery("stores", fetchStores, {
+  } = useInfiniteQuery(["stores", searchParams], fetchStores, {
     getNextPageParam: (lastPage: any) =>
       lastPage.data?.length > 0 ? lastPage.page + 1 : undefined,
   });
@@ -76,7 +82,7 @@ export default function StoreListPage() {
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
       {/* search filter */}
-      <SearchFilter />
+      <SearchFilter setQ={setQ} setDistirct={setDistrict} />
       <ul role="list" className="divide-y divide-gray-100">
         {isLoading ? (
           <Loading />
