cadc0bb40a87549da30ad6713f6421ef2c88b372
oh-ji-yeon <o.delayed0106@gmail.com>
2024-03-22 14:57:25 +0900

    찜한 맛집 모아보기 작업

--- src/pages/users/likes.tsx
+++ src/pages/users/likes.tsx
@@ -1,7 +1,54 @@
+import Loading from "@/components/Loading";
+import StoreList from "@/components/StoreList";
+import { LikeApiResponse, LikeInterface } from "@/interface";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { useRouter } from "next/router";
+import Pagination from "@/components/Pagination";
+
 export default function LikesPage() {
+  const router = useRouter();
+  const { page = "1" }: any = router.query;
+
+  const fetchLikes = async () => {
+    const { data } = await axios(`/api/likes?limit=10&page=${page}`);
+    return data as LikeApiResponse;
+  };
+
+  const {
+    data: likes,
+    isError,
+    isLoading,
+  } = useQuery(`likes-${page}`, fetchLikes);
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요.
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Likes Page</h1>
+    <div className="px-4 md:max-w-4xl mx-auto py-8">
+      <h3 className="text-lg font-semibold">찜한 맛집📍</h3>
+      <div className="mt-1 text-gray-500 text-sm">찜한 가게 리스트입니다.</div>
+      <ul role="list" className="divide-y divide-gray-100 mt-10">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          likes?.data.map((Like: LikeInterface, index) => (
+            <StoreList i={index} store={Like.store} key={index} />
+          ))
+        )}
+      </ul>
+      {likes?.totalPage && likes?.totalPage > 0 && (
+        <Pagination
+          total={likes?.totalPage}
+          page={page}
+          pathname="/users/likes"
+        />
+      )}
     </div>
   );
 }
